7dc1e653ced9b2426b267e14a1781a91798de819
ShopOne <oneky8080@gmail.com>
2020-03-27 18:34:12 +0900

    add jsoup

--- app/src/main/java/com/support/atcoderusers/MainActivity.kt
+++ app/src/main/java/com/support/atcoderusers/MainActivity.kt
@@ -2,6 +2,8 @@ package com.support.atcoderusers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,6 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        webView.apply{
+            settings.apply{
+                javaScriptEnabled = true
+            }
+        }
         webView.loadUrl("https://example.com")
     }
 }
