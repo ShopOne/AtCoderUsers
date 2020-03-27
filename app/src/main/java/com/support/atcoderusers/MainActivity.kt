b881e6c99ec85e82c2a57a6b4d6a2cc00117b0a1
ShopOne <oneky8080@gmail.com>
2020-03-27 14:46:36 +0900

    Initial commit

---
+++ app/src/main/java/com/support/atcoderusers/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.support.atcoderusers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView.loadUrl("https://example.com")
+    }
+}
